0d7b8175ecb17a9e43a9c7d1f93ec282ac43d675
CJskii <czareczarek3@gmail.com>
2022-08-31 03:48:19 +0100

    dynamic card

--- script.js
+++ script.js
@@ -25,39 +25,60 @@ function setAtribute(){
   let card = document.createElement('div')
   card.setAttribute('class', 'card')
   mainWrapper.appendChild(card)
+  // to card
+  let header = document.createElement('div')
+  header.setAttribute('class', 'modify-buttons')
+  let headerText = document.createElement('button')
+  headerText.setAttribute("class", 'edit' )
+  headerText.setAttribute("type", 'text' )
+  headerText.textContent = "Enter below"
+  card.appendChild(header)
+  header.appendChild(headerText)
+
   //title
   let title = document.createElement('div')
   title.setAttribute('class', 'title')
+  let titleSpan = document.createElement('span')
+  titleSpan.textContent = "Book:"
   let titleInput = document.createElement('input')
+  titleInput.setAttribute('type', 'text')
+  title.appendChild(titleSpan)
   title.appendChild(titleInput)
   card.appendChild(title)
   //author
   let author = document.createElement('div')
-  title.setAttribute('class', 'author')
+  author.setAttribute('class', 'author')
   let authorInput = document.createElement('input')
+  authorInput.setAttribute('type' , 'author')
   author.appendChild(authorInput)
   card.appendChild(author)
   // book-info
   let bookInfo = document.createElement('div')
   bookInfo.setAttribute('class', 'book-info')
   card.appendChild(bookInfo);
+  // page
   let pageSpan = document.createElement('span')
   pageSpan.textContent = "666"
   bookInfo.appendChild(pageSpan)
+  //button wrap
   let confirmWrap = document.createElement('div')
   confirmWrap.setAttribute('class', 'confirm-wrap')
   bookInfo.appendChild(confirmWrap)
+  // confirm btn
   let confirmBtn = document.createElement('button')
   confirmBtn.setAttribute('class', 'confirm')
   confirmBtn.textContent = '✓'
   confirmWrap.appendChild(confirmBtn)
+  // cancel btn
   let cancelBtn = document.createElement('button')
   cancelBtn.setAttribute('class', 'cancel')
   cancelBtn.textContent = 'X'
   confirmWrap.appendChild(cancelBtn)
+  // read 
   let readSpan = document.createElement('span')
   readSpan.textContent = "Yes"
   bookInfo.appendChild(readSpan)
+  console.log(card)
 }
 
 function appendElements(){
